b55f784e3efa3de9fb364589eb56d9bc38434fa1
laixiangran <laixiangran@163.com>
2017-04-23 17:43:45 +0800

    v1.0.2

--- index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Created by laixiangran on 2017/4/23.
- * homepage：http://www.laixiangran.cn
- */
-
-export {EssenceNg2ChartModule} from './src/essence-ng2-chart.module';
-export {EssenceChart} from './src/EssenceChart';
