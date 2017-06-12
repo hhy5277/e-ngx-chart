eee75654de48fba5005a1bc6419f702a51365689
laixiangran <laixiangran@163.com>
2017-06-12 16:26:02 +0800

    丰富示例

--- example/app/app.component.ts
+++ example/app/app.component.ts
@@ -33,6 +33,18 @@ export class AppComponent {
                 align: "center"
             }
         },
+        plotOptions: {
+            series: {
+                marker: {
+                    enabled: false,
+                    states: {
+                        hover: {
+                            enabled: false
+                        }
+                    }
+                }
+            }
+        },
         series: [{
             data: [100, 120, 130, 140, 250, 160, 230, 180]
         }],
@@ -93,7 +105,7 @@ export class AppComponent {
         },
         series: [{
             data: [
-                ["西冉村",100],
+                ["西冉村", 100],
                 ["宝山", 120],
                 ["苗寨", 130],
                 ["四季青", 140],
@@ -114,19 +126,33 @@ export class AppComponent {
         }
     });
 
-    setData() {
-        let data: number[] = [100, 120, 130, 140, 250, 160, 230, 180];
-        data.forEach((d: number, index: number) => {
-            data[index] = d + Math.random() * 100;
-        });
-        this.chart.setData(data);
-        this.chart1.setData(data);
-        this.chart2.setData(data);
+    chartReady($event: any) {
+        setInterval(() => {
+            let data: number[] = [100, 120, 130, 140, 250, 160, 230, 180];
+            data.forEach((d: number, index: number) => {
+                data[index] = d + Math.random() * 100;
+            });
+            this.chart.setData(data);
+        }, 3000);
     }
 
-    setXCategories() {
-        let categories: string[] = ["金河闸", "田村", "香山", "西冉村", "宝山", "苗寨", "四季青", "北坞村"];
-        this.chart.setXCategories(categories);
-        this.chart1.setXCategories(categories);
+    chart1Ready($event: any) {
+        setInterval(() => {
+            let data: number[] = [100, 120, 130, 140, 250, 160, 230, 180];
+            data.forEach((d: number, index: number) => {
+                data[index] = d + Math.random() * 150;
+            });
+            this.chart1.setData(data);
+        }, 3000);
+    }
+
+    chart2Ready($event: any) {
+        setInterval(() => {
+            let data: number[] = [100, 120, 130, 140, 250, 160, 230, 180];
+            data.forEach((d: number, index: number) => {
+                data[index] = d + Math.random() * 200;
+            });
+            this.chart2.setData(data);
+        }, 3000);
     }
 }
